c69c43d01b1275d784fb7e4bef4798f2d002cab1
frankzha <frank.zhang@gmail.com>
2017-10-12 13:13:53 -0700

    Add sql files used in localdb

---
+++ UploadCsv/UploadCsv/Sql/AddCsvFile.sql
@@ -0,0 +1,22 @@
+﻿USE [CsvDb]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP PROCEDURE [dbo].[AddCsvFile];
+
+
+GO
+CREATE PROCEDURE [dbo].[AddCsvFile]
+	@fileName nvarchar(128),
+	@fileContent varbinary(MAX)
+AS
+	INSERT INTO dbo.CsvFile
+	([FileName], [FileContent])
+	VALUES
+	(@fileName, @fileContent)
+RETURN 0
